c84df71e1a94d120aab70da7bfd9a07ec1b32bbb
Mateus-amc <68255115+Mateus-amc@users.noreply.github.com>
2022-03-31 08:43:15 -0300

    Update semaforo.ino

--- semaforo.ino
+++ semaforo.ino
@@ -5,11 +5,13 @@
 
 void setup() 
 {
-  
+  // Setar os pinos dos leds para serem OUTPUT (saída)
   pinMode(vermelho, OUTPUT);
   pinMode(amarelo, OUTPUT);
   pinMode(verde, OUTPUT);
 
+  // Setar todos os leds como desligados no começo (não é obrigatório mas apenas para 
+  // garantir que começará tudo desligado)
   digitalWrite(vermelho, LOW);
   digitalWrite(amarelo, LOW);
   digitalWrite(verde, LOW);
@@ -18,14 +20,17 @@ void setup()
 
 void loop() 
 {
-
-  digitalWrite(vermelho, HIGH);
-  delay(4000);
   
+  // sinal vermelho 
+  digitalWrite(vermelho, HIGH);
+  delay(4000); // tempo que cada sinal ficará ligado (em ms)
+  
+  // sinal verde
   digitalWrite(vermelho, LOW);
   digitalWrite(verde, HIGH);
   delay(4000);
   
+  // sinal amarelo
   digitalWrite(verde, LOW);
   digitalWrite(amarelo, HIGH);
   delay(4000);
